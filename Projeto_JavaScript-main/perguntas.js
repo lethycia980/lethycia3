0b872e187b0d3120b60a82720fd154b5e493688b
ʟᴇᴛʜʏᴄɪᴀ <137515357+lethycia980@users.noreply.github.com>
2024-10-08 08:31:03 -0300

    Add files via upload

---
+++ Projeto_JavaScript-main/perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é Pyton?',
+    'O Python é uma linguagem de programação',
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    "A capital é Paris",
+)
+criaCartao(
+    "Programação",
+    "O que é uma função?",
+    "Uma função é um bloco de código que executa alguma tarefa",
+)
+criaCartao(
+    ' Lingua Inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em ingles é HI',
+)
